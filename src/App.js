101a2e6d4d1ba3cdb0bce1b37154efd4026f74af
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-10-02 13:05:06 -0600

    final commit before standup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    user: {},
+    userText: "ChrisRDaniels",
+    userFollowers: []
+  };
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/ChrisRDaniels`)
+    .then(res => {
+      this.setState({
+        user: res.data
+      })
+      console.log(res)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+
+    axios.get(`https://api.github.com/users/ChrisRDaniels/followers`)
+    .then(res => {
+      console.log(res)
+      this.setState({
+        userFollowers: res.data
+      })
+      console.log(this.state.userFollowers)
+    })
+  }
+
+  handleChanges = e => {
+    this.setState({
+      userText: e.target.value
+    })
+  }
+
+  fetchUser = e => {
+    e.preventDefault()
+    axios.get(`https://api.github.com/users/${this.state.userText}`)
+    .then(res => {
+      this.setState({
+        user: res.data
+      })
+      console.log(res)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+
+    axios.get(`https://api.github.com/users/${this.state.userText}/followers`)
+    .then(res => {
+      console.log(res)
+      this.setState({
+        userFollowers: res.data
+      })
+      console.log(this.state.userFollowers)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className='get-user'>
+          <input type='text' value={this.state.userText} onChange={this.handleChanges} />
+          <button onClick={this.fetchUser}>Lookup User</button>
+        </div>
+        <div className="user-card">
+          <h2>{this.state.user.name}</h2>
+          <p>Bio: {this.state.user.bio}</p>
+          <p>Following: {this.state.user.following}</p>
+          <p>Followers: {this.state.user.followers}</p>
+        </div>
+        <h2>{this.state.user.name}'s Followers</h2>
+        <div className="follower-list">
+          {this.state.userFollowers.map(item => 
+            <div className='follower-card'>
+              <img src={item.avatar_url} />
+              <h4>{item.login}</h4>
+              <p>{item.html_url}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
