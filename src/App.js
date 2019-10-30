a826c0df3c804b50c0e8a95ecc047f0dfc52e839
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-10-30 14:09:04 -0600

    final commit before standup

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends React.Component {
           <p>Bio: {this.state.user.bio}</p>
           <p>Following: {this.state.user.following}</p>
           <p>Followers: {this.state.user.followers}</p>
-          <img src="http://ghchart.rshah.org/ChrisRDaniels" alt="ChrisRDaniels's Github chart" width ="700px" height="250px" />
+          <img src="http://ghchart.rshah.org/ChrisRDaniels" alt="ChrisRDaniels's Github chart"/>
         </div>
         <h2>{this.state.user.name}'s Followers</h2>
         <div className="follower-list">
